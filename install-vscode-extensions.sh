6db816289d28566f9a992fb7926982a1d9c8b34e
Chabam <fchabot1337@gmail.com>
2018-08-05 01:50:57 -0400

    Added some changes to vs code

---
+++ install-vscode-extensions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+code --install-extension alefragnani.project-manager
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension justusadam.language-haskell
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension octref.vetur
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension shardulm94.trailing-spaces
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension yzhang.markdown-all-in-one
