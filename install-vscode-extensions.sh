0e167d8991e601d25b135c9b9e5356327995690a
Chabam <fchabot1337@gmail.com>
2019-08-20 14:15:59 -0400

    VSCode settings update

--- install-vscode-extensions.sh
+++ install-vscode-extensions.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension eamodio.gitlens
 code --install-extension James-Yu.latex-workshop
 code --install-extension ms-vscode.cpptools
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension robertohuertasm.vscode-icons
 code --install-extension shardulm94.trailing-spaces
 code --install-extension vadimcn.vscode-lldb
+code --install-extension vscode-icons-team.vscode-icons
 code --install-extension wayou.vscode-todo-highlight
 code --install-extension yzhang.markdown-all-in-one
---
+++ update-vscode-extensions.sh
@@ -0,0 +1,1 @@
+code --list-extensions | xargs -L 1 echo code --install-extension > ./install-vscode-extensions.sh
