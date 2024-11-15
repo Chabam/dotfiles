c5a37aa8dbe118cfb4f0a1777bf9e014222bf75f
Chabam <fchabot1337@gmail.com>
2024-11-15 14:33:17 -0500

    Colas

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -445,7 +445,7 @@ require("lazy").setup({
 
 			-- Useful status updates for LSP.
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ "j-hui/fidget.nvim", opts = {} },
+			{ "j-hui/fidget.nvim", opts = { notification = { window = { winblend = 0 } } } },
 
 			-- Allows extra capabilities provided by nvim-cmp
 			"hrsh7th/cmp-nvim-lsp",
@@ -833,13 +833,17 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
+		"navarasu/onedark.nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
+		opts = {
+			transparent = true,
+			style = "deep",
+		},
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("onedark")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
