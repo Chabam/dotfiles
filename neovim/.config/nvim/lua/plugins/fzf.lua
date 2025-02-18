cfa867648aae50fbf648a16b3be9a8668e408e40
Chabam <fchabot1337@gmail.com>
2025-02-18 19:25:28 +0100

    Some fixes

--- neovim/.config/nvim/lua/plugins/fzf.lua
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -1,59 +1,59 @@
 return {
-    "ibhagwan/fzf-lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        local fzf = require("fzf-lua")
-        fzf.setup({
-            fzf_opts = {
-                ["--pointer"] = " ",
-                ["--separator"] = " ",
-            },
-            fzf_colors = {
-                ["fg"]      = { "fg", "Comment" },
-                ["bg"]      = { "bg", "palette.bg0" },
-                ["hl"]      = { "fg", "Normal" },
-                ["fg+"]     = { "fg", "Comment", "bold" },
-                ["bg+"]     = { "bg", { "palette.sel0", "Normal" } },
-                ["hl+"]     = { "fg", "Normal" },
-                ["info"]    = { "fg", "NonText" },
-                ["prompt"]  = { "fg", "FzfLuaBufLineNr" },
-                ["spinner"] = { "fg", "FzfLuaBufNr" },
-                ["header"]  = { "fg", "Normal" },
-                ["query"]   = { "fg", "palette.green" },
-                ["gutter"]  = "-1",
-            },
-            winopts = {
-                backdrop = false,
-                height = 0.7,
-                width = 0.65,
-                preview = {
-                    layout = "vertical"
-                },
-            }
-        })
-        fzf.register_ui_select()
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local fzf = require("fzf-lua")
+		fzf.setup({
+			fzf_opts = {
+				["--pointer"] = " ",
+				["--separator"] = " ",
+			},
+			fzf_colors = {
+				["fg"] = { "fg", "Comment" },
+				["bg"] = { "bg", "palette.bg0" },
+				["hl"] = { "fg", "Normal" },
+				["fg+"] = { "fg", "Comment", "bold" },
+				["bg+"] = { "bg", { "palette.sel0", "Normal" } },
+				["hl+"] = { "fg", "Normal" },
+				["info"] = { "fg", "NonText" },
+				["prompt"] = { "fg", "FzfLuaBufLineNr" },
+				["spinner"] = { "fg", "FzfLuaBufNr" },
+				["header"] = { "fg", "Normal" },
+				["query"] = { "fg", "palette.green" },
+				["gutter"] = "-1",
+			},
+			winopts = {
+				backdrop = false,
+				height = 0.7,
+				width = 0.65,
+				preview = {
+					layout = "vertical",
+				},
+			},
+		})
+		fzf.register_ui_select()
 
-        vim.keymap.set("n", "<leader>sh", fzf.helptags, { desc = "[S]earch [H]elp" })
-        vim.keymap.set("n", "<leader>sk", fzf.keymaps, { desc = "[S]earch [K]eymaps" })
-        vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
-        vim.keymap.set("n", "<leader>ss", fzf.builtin, { desc = "[S]earch [S]elect fzf" })
-        vim.keymap.set("n", "<leader>sw", fzf.grep_cword, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>sg", fzf.live_grep_native, { desc = "[S]earch by [G]rep" })
-        vim.keymap.set("n", "<leader>sd", fzf.diagnostics_document, { desc = "[S]earch [D]iagnostics" })
-        vim.keymap.set("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
-        vim.keymap.set("n", "<leader>s.", fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-        vim.keymap.set("n", "<leader>sb", fzf.buffers, { desc = "[S]earch existing [B]uffers" })
-        --
-        vim.keymap.set("n", "<leader>/", fzf.lgrep_curbuf, { desc = "[/] Fuzzily search in current buffer" })
-        vim.keymap.set("n", "<leader>s/", function()
-            fzf.live_grep({
-                grep_open_files = true,
-                prompt_title = "Live Grep in Open Files",
-            })
-        end, { desc = "[S]earch [/] in Open Files" })
+		vim.keymap.set("n", "<leader>sh", fzf.helptags, { desc = "[S]earch [H]elp" })
+		vim.keymap.set("n", "<leader>sk", fzf.keymaps, { desc = "[S]earch [K]eymaps" })
+		vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>ss", fzf.builtin, { desc = "[S]earch [S]elect fzf" })
+		vim.keymap.set("n", "<leader>sw", fzf.grep_cword, { desc = "[S]earch current [W]ord" })
+		vim.keymap.set("n", "<leader>sg", fzf.live_grep_native, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>sd", fzf.diagnostics_document, { desc = "[S]earch [D]iagnostics" })
+		vim.keymap.set("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
+		vim.keymap.set("n", "<leader>s.", fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		vim.keymap.set("n", "<leader>sb", fzf.buffers, { desc = "[S]earch existing [B]uffers" })
+		--
+		vim.keymap.set("n", "<leader>/", fzf.lgrep_curbuf, { desc = "[/] Fuzzily search in current buffer" })
+		vim.keymap.set("n", "<leader>s/", function()
+			fzf.live_grep({
+				grep_open_files = true,
+				prompt_title = "Live Grep in Open Files",
+			})
+		end, { desc = "[S]earch [/] in Open Files" })
 
-        vim.keymap.set("n", "<leader>sn", function()
-            fzf.files({ cwd = vim.fn.stdpath("config") })
-        end, { desc = "[S]earch [N]eovim files" })
-    end
+		vim.keymap.set("n", "<leader>sn", function()
+			fzf.files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "[S]earch [N]eovim files" })
+	end,
 }
--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,159 +1,161 @@
 return {
-    {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {},
-    },
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "VeryLazy",
-        opts = {
-            hint_enable = false,
-        },
-        config = function(_, opts) require 'lsp_signature'.setup(opts) end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            { "williamboman/mason.nvim", config = true },
-            "williamboman/mason-lspconfig.nvim",
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
-            {
-                "j-hui/fidget.nvim",
-                opts = {
-                    notification = {
-                        window = { winblend = 0 }
-                    }
-                }
-            },
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        config = function()
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-                callback = function(event)
-                    local map = function(keys, func, desc, mode)
-                        mode = mode or "n"
-                        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-                    end
-                    local fzf = require("fzf-lua")
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {},
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {
+			hint_enable = false,
+		},
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{ "williamboman/mason.nvim", config = true },
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{
+				"j-hui/fidget.nvim",
+				opts = {
+					notification = {
+						window = { winblend = 0 },
+					},
+				},
+			},
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+					local fzf = require("fzf-lua")
 
-                    local lsp_fzf_opts = {
-                        jump_to_single_result = true
-                    }
+					local lsp_fzf_opts = {
+						jump1 = true,
+					}
 
-                    map("gd", function()
-                        fzf.lsp_definitions(lsp_fzf_opts)
-                    end, "[G]oto [D]efinition")
-                    map("gr", function()
-                        fzf.lsp_references(lsp_fzf_opts)
-                    end, "[G]oto [R]eferences")
-                    map("gI", function()
-                        fzf.lsp_implementations(lsp_fzf_opts)
-                    end, "[G]oto [I]mplementation")
-                    map("<leader>D", function()
-                        fzf.lsp_typedefs(lsp_fzf_opts)
-                    end, "Type [D]efinition")
-                    map("<leader>ds", fzf.lsp_document_symbols, "[D]ocument [S]ymbols")
-                    map("<leader>ws", fzf.lsp_workspace_symbols, "[W]orkspace [S]ymbols")
+					map("gd", function()
+						fzf.lsp_definitions(lsp_fzf_opts)
+					end, "[G]oto [D]efinition")
+					map("gr", function()
+						fzf.lsp_references(lsp_fzf_opts)
+					end, "[G]oto [R]eferences")
+					map("gI", function()
+						fzf.lsp_implementations(lsp_fzf_opts)
+					end, "[G]oto [I]mplementation")
+					map("<leader>D", function()
+						fzf.lsp_typedefs(lsp_fzf_opts)
+					end, "Type [D]efinition")
+					map("<leader>ds", fzf.lsp_document_symbols, "[D]ocument [S]ymbols")
+					map("<leader>ws", fzf.lsp_workspace_symbols, "[W]orkspace [S]ymbols")
 
-                    map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-                    map("<leader>ca", require("fzf-lua").lsp_code_actions, "[C]ode [A]ction", { "n", "x" })
+					map("<leader>ca", require("fzf-lua").lsp_code_actions, "[C]ode [A]ction", { "n", "x" })
 
-                    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-                    map("gH", function()
-                        vim.cmd("ClangdSwitchSourceHeader")
-                    end, "[G]oto [H]eader", { "n", "x" })
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("gH", function()
+						vim.cmd("ClangdSwitchSourceHeader")
+					end, "[G]oto [H]eader", { "n", "x" })
 
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-                        local highlight_augroup =
-                            vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-                        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                            buffer = event.buf,
-                            group = highlight_augroup,
-                            callback = vim.lsp.buf.document_highlight,
-                        })
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+						local highlight_augroup =
+							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.document_highlight,
+						})
 
-                        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                            buffer = event.buf,
-                            group = highlight_augroup,
-                            callback = vim.lsp.buf.clear_references,
-                        })
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.clear_references,
+						})
 
-                        vim.api.nvim_create_autocmd("LspDetach", {
-                            group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-                            callback = function(event2)
-                                vim.lsp.buf.clear_references()
-                                vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-                            end,
-                        })
-                    end
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							end,
+						})
+					end
 
-                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-                        map("<leader>th", function()
-                            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-                        end, "[T]oggle Inlay [H]ints")
-                    end
-                end,
-            })
+					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+						end, "[T]oggle Inlay [H]ints")
+					end
+				end,
+			})
 
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-            capabilities.textDocument.completion.completionItem.snippetSupport = false
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			capabilities.textDocument.completion.completionItem.snippetSupport = false
 
-            local servers = {
-                clangd = {},
-                ltex = {
-                    settings = {
-                        ltex = {
-                            language = "fr",
-                            completionEnabled = true,
-                        },
-                    },
-                    filetypes = { "tex", "markdown" },
-                    enabled = { "tex", "markdown" },
-                },
-                pyright = {
-                    filetypes = { "python" },
-                },
-                neocmake = {
-                    filetpes = { "cmake", "CMakeLists.txt" },
-                },
-                lua_ls = {
-                    settings = {
-                        Lua = {
-                            completion = {
-                                callSnippet = "Replace",
-                            },
-                        },
-                    },
-                    filetypes = { "lua" },
-                },
-            }
+			local servers = {
+				clangd = {},
+				ltex = {
+					settings = {
+						ltex = {
+							language = "fr",
+							completionEnabled = true,
+						},
+					},
+					filetypes = { "tex", "markdown" },
+					enabled = { "tex", "markdown" },
+				},
+				pyright = {
+					filetypes = { "python" },
+				},
+				neocmake = {
+					filetpes = { "cmake", "CMakeLists.txt" },
+				},
+				lua_ls = {
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+					filetypes = { "lua" },
+				},
+			}
 
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                automatic_installation = false,
-                ensure_installed = {
-                    "clangd",
-                    "ltex",
-                    "pyright",
-                    "neocmake",
-                    "lua_ls",
-                },
-                handlers = {
-                    function(server_name)
-                        local server_config = servers[server_name] or {}
-                        server_config.capabilities = vim.tbl_deep_extend("force", {}, capabilities,
-                            server_config.capabilities or {})
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				automatic_installation = false,
+				ensure_installed = {
+					"clangd",
+					"ltex",
+					"pyright",
+					"neocmake",
+					"lua_ls",
+				},
+				handlers = {
+					function(server_name)
+						local server_config = servers[server_name] or {}
+						server_config.capabilities =
+							vim.tbl_deep_extend("force", {}, capabilities, server_config.capabilities or {})
 
-                        local server = require("lspconfig")[server_name]
-                        server.setup(server_config)
-                    end,
-                },
-            })
-        end,
-    }
+						local server = require("lspconfig")[server_name]
+						server.setup(server_config)
+					end,
+				},
+			})
+		end,
+	},
 }
