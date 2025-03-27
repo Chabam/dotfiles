0ffe2beacf0612f8878d489d5576e9772e3a60bb
Chabam <fchabot1337@gmail.com>
2025-03-27 11:39:31 -0400

    Fixed latexindent

--- neovim/.config/nvim/lua/plugins/autoformat.lua
+++ neovim/.config/nvim/lua/plugins/autoformat.lua
@@ -17,6 +17,7 @@ return {
             notify_on_error = false,
             formatters_by_ft = {
                 lua = { "stylua" },
+                tex = { "latexindent" },
                 cpp = { "clang_format" },
             },
             formatters = {
