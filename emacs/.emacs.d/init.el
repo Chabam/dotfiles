342c20a22579c4f961869a21b8b9719119ce08c6
Chabam <fchabot1337@gmail.com>
2025-07-31 13:33:55 -0400

    Auto install treesit grammars

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -186,6 +186,8 @@
   (dired-dwim-target t))
 
 (use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode)
