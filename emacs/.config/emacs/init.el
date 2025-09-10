5d94af64adeb4a915ce25f63429015edcab51800
Chabam <fchabot1337@gmail.com>
2025-09-10 09:37:22 -0400

    Added a proper modeline (thanks Prot!)

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -16,7 +16,7 @@
     (org-agenda-list)
     (delete-other-windows)))
 
-;; Thanks prot!
+;; Thanks Prot!
 (defun prot-spell-ispell-display-buffer (buffer)
   "Function to override `ispell-display-buffer' for BUFFER.
 Use this as `advice-add' to override the aforementioned Ispell
@@ -36,6 +36,7 @@ function.  Then you can control the buffer's specifics via
          ("C-x C-c" . nil)              ; Closing emacs 🙂 (actually gets rebound later)
          ("M-`" . nil)                  ; menu bar in the minibuffer
          ;; Rebinds
+         ("M-o" . other-window)
          ("C-x C-c C-c" . save-buffers-kill-emacs)
          ("C-x C-r" . restart-emacs)
          ("M-z" . zap-up-to-char)       ; zap-up-to-char instead of zap-to-char
@@ -119,9 +120,6 @@ function.  Then you can control the buffer's specifics via
   (setq tab-bar-new-button-show nil)
   (setq tab-bar-show 1)
 
-  ;; Popping the mark doesn't need additional `C-x'
-  (setq set-mark-command-repeat-pop t)
-
   (setq deleted-by-moving-to-trash t)
 
   ;; Enable only git for VC
@@ -184,8 +182,6 @@ function.  Then you can control the buffer's specifics via
 	      ;; Blue modeline
 	      (bg-mode-line-active bg-blue-intense)
 	      (fg-mode-line-active fg-main)
-	      (border-mode-line-active unspecified)
-	      (border-mode-line-inactive unspecified)
 
 	      ;; Number line invisible
 	      (fringe unspecified)
@@ -220,12 +216,254 @@ function.  Then you can control the buffer's specifics via
   (setq modus-themes-bold-constructs t)
   (setq modus-themes-mixed-fonts t))
 
+;; Modeline  ===================================================================
+;; Most stuff here comes from Prot
+
+(defface chbm-modeline-green-bg
+  (modus-themes-with-colors
+    `((t :inherit bold :foreground ,bg-green-nuanced :background ,bg-green-intense :box ,green-intense)))
+  "Face for modeline indicators with a background."
+  :group 'chbm-modeline-faces)
+
+(defface chbm-modeline-magenta-bg
+  (modus-themes-with-colors
+    `((t :inherit bold :foreground ,magenta-intense :background ,bg-magenta-nuanced :box ,magenta-intense)))
+  "Face for modeline indicators with a background."
+  :group 'chbm-modeline-faces)
+
+(defface chbm-modeline-lavender-bg
+  (modus-themes-with-colors
+    `((t :inherit bold :foreground ,fg-lavender :background ,bg-lavender :box ,fg-lavender)))
+  "Face for modeline indicators with a background."
+  :group 'chbm-modeline-faces)
+
+(defface chbm-modeline-red-fg
+  (modus-themes-with-colors
+    `((t :foreground ,red)))
+  "Face for modeline indicators with a background."
+  :group 'chbm-modeline-faces)
+
+(defvar-local chbm-modeline-kbd-macro
+    '(:eval
+      (when (and (mode-line-window-selected-p) defining-kbd-macro)
+        (propertize " KMacro " 'face 'chbm-modeline-green-bg)))
+  "Mode line construct displaying `mode-line-defining-kbd-macro'.
+Specific to the current window's mode line.")
+
+(defun chbm-modeline-buffer-name ()
+  "Return buffer name, with read-only indicator if relevant."
+  (let ((name (buffer-name)))
+    (if buffer-read-only
+        (format "%s %s" (char-to-string #xE0A2) name)
+      name)))
+
+(defun chbm-modeline-buffer-identification-face ()
+  "Return appropriate face or face list for `chbm-modeline-buffer-identification'."
+  (let ((file (buffer-file-name)))
+    (cond
+     ((and (mode-line-window-selected-p)
+           file
+           (buffer-modified-p))
+      '(italic mode-line-buffer-id))
+     ((and file (buffer-modified-p))
+      'italic)
+     ((mode-line-window-selected-p)
+      'mode-line-buffer-id))))
+
+(defvar-local chbm-modeline-buffer-identification
+    '(:eval
+      (propertize (chbm-modeline-buffer-name)
+                  'face (chbm-modeline-buffer-identification-face)
+                  'mouse-face 'mode-line-highlight
+                  'help-echo (chbm-modeline-buffer-name-help-echo)))
+  "Mode line construct for identifying the buffer being displayed.
+Propertize the current buffer with the `mode-line-buffer-id'
+face.  Let other buffers have no face.")
+
+(defun chbm-modeline-buffer-name-help-echo ()
+  "Return `help-echo' value for `chbm-modeline-buffer-identification'."
+  (concat
+   (propertize (buffer-name) 'face 'mode-line-buffer-id)
+   "\n"
+   (propertize
+    (or (buffer-file-name)
+        (format "No underlying file.\nDirectory is: %s" default-directory))
+    'face 'font-lock-doc-face)))
+
+(defvar-local chbm-modeline-remote-status
+    '(:eval
+      (when (file-remote-p default-directory)
+        (propertize " @ "
+                    'face 'chbm-modeline-red
+                    'mouse-face 'mode-line-highlight)))
+  "Mode line construct for showing remote file name.")
+
+(defvar-local chbm-modeline-window-dedicated-status
+    '(:eval
+      (when (window-dedicated-p)
+        (propertize " = "
+                    'face 'chbm-modeline-lavender-bg
+                    'mouse-face 'mode-line-highlight)))
+  "Mode line construct for dedicated window indicator.")
+
+(defun chbm-modeline-major-mode-indicator ()
+  "Return appropriate propertized mode line indicator for the major mode."
+  (let ((indicator (cond
+                    ((derived-mode-p 'text-mode) "§")
+                    ((derived-mode-p 'prog-mode) "λ")
+                    ((derived-mode-p 'comint-mode) ">_")
+                    (t "◦"))))
+    (propertize indicator 'face 'shadow)))
+
+(defun chbm-modeline-major-mode-name ()
+  "Return capitalized `major-mode' without the -mode suffix."
+  (capitalize (string-replace "-mode" "" (symbol-name major-mode))))
+
+(defun chbm-modeline-major-mode-help-echo ()
+  "Return `help-echo' value for `chbm-modeline-major-mode'."
+  (if-let* ((parent (get major-mode 'derived-mode-parent)))
+      (format "Symbol: `%s'.  Derived from: `%s'" major-mode parent)
+    (format "Symbol: `%s'." major-mode)))
+
+(defvar-local chbm-modeline-major-mode
+    (list
+     (propertize "%[" 'face 'chbm-modeline-red-fg)
+     '(:eval
+       (propertize (concat
+                    (chbm-modeline-major-mode-indicator)
+                    " "
+                    (chbm-modeline-major-mode-name))
+                   'mouse-face 'mode-line-highlight
+                   'help-echo (chbm-modeline-major-mode-help-echo)))
+     (propertize "%]" 'face 'chbm-modeline-red-fg))
+  "Mode line construct for displaying major modes.")
+
+(declare-function vc-git--symbolic-ref "vc-git" (file))
+
+(defun chbm-modeline--vc-branch-name (file backend)
+  "Return capitalized VC branch name for FILE with BACKEND."
+  (when-let* ((rev (vc-working-revision file backend))
+              (branch (or (vc-git--symbolic-ref file)
+                          (substring rev 0 7))))
+    (capitalize branch)))
+
+(declare-function vc-git-working-revision "vc-git" (file))
+
+(defvar chbm-modeline-vc-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [mode-line down-mouse-1] 'vc-diff)
+    (define-key map [mode-line down-mouse-3] 'vc-root-diff)
+    map)
+  "Keymap to display on VC indicator.")
+
+(defun chbm-modeline--vc-help-echo (file)
+  "Return `help-echo' message for FILE tracked by VC."
+  (format "Revision: %s\nmouse-1: `vc-diff'\nmouse-3: `vc-root-diff'"
+          (vc-working-revision file)))
+
+(defun chbm-modeline--vc-text (file branch &optional face)
+  "Prepare text for Git controlled FILE, given BRANCH.
+With optional FACE, use it to propertize the BRANCH."
+  (concat
+   (propertize (char-to-string #xE0A0) 'face 'shadow)
+   " "
+   (propertize branch
+               'face face
+               'mouse-face 'mode-line-highlight
+               'help-echo (chbm-modeline--vc-help-echo file)
+               'local-map chbm-modeline-vc-map)))
+
+(defun chbm-modeline--vc-details (file branch &optional face)
+  "Return Git BRANCH details for FILE, truncating it if necessary.
+The string is truncated if the width of the window is smaller
+than `split-width-threshold'."
+  (chbm-modeline--vc-text file branch face))
+
+(defvar chbm-modeline--vc-faces
+  '((added . vc-locally-added-state)
+    (edited . vc-edited-state)
+    (removed . vc-removed-state)
+    (missing . vc-missing-state)
+    (conflict . vc-conflict-state)
+    (locked . vc-locked-state)
+    (up-to-date . vc-up-to-date-state))
+  "VC state faces.")
+
+(defun chbm-modeline--vc-get-face (key)
+  "Get face from KEY in `chbm-modeline--vc-faces'."
+  (alist-get key chbm-modeline--vc-faces 'vc-up-to-date-state))
+
+(defun chbm-modeline--vc-face (file backend)
+  "Return VC state face for FILE with BACKEND."
+  (when-let* ((key (vc-state file backend)))
+    (chbm-modeline--vc-get-face key)))
+
+(defvar-local chbm-modeline-vc-branch
+    '(:eval
+      (when-let* (((mode-line-window-selected-p))
+                  (file (or buffer-file-name default-directory))
+                  (backend (or (vc-backend file) 'Git))
+                  (branch (chbm-modeline--vc-branch-name file backend))
+                  (face (chbm-modeline--vc-face file backend)))
+        (chbm-modeline--vc-details file branch face)))
+  "Mode line construct to return propertized VC branch.")
+
+(defvar-local chbm-modeline-diagnostics
+    '(:eval
+      (when-let* (((mode-line-window-selected-p))
+                  (eglot '(eglot--managed-mode ("[" eglot--mode-line-format "]")))
+                  (flymake (if (bound-and-true-p flymake-mode)
+                               '(flymake-mode-line-exception flymake-mode-line-counters)
+                             nil)))
+        (list eglot flymake))
+      "Mode line construct to return propertized buffer diagnostics."))
+
+
+(dolist (construct '(chbm-modeline-kbd-macro
+                     chbm-modeline-remote-status
+                     chbm-modeline-buffer-identification
+                     chbm-modeline-window-dedicated-status
+                     chbm-modeline-major-mode
+                     chbm-modeline-vc-branch
+                     chbm-modeline-diagnostics
+                     ))
+  (put construct 'risky-local-variable t))
+
+(setq-default mode-line-format
+              '("%e"
+                chbm-modeline-kbd-macro
+                chbm-modeline-remote-status
+                chbm-modeline-window-dedicated-status
+                "  "
+                chbm-modeline-buffer-identification
+                "  "
+                chbm-modeline-major-mode
+                mode-line-process
+
+                mode-line-format-right-align
+
+                chbm-modeline-diagnostics
+                "  "
+                chbm-modeline-vc-branch
+                " %p %l:%c  "
+                ))
+
 ;; Frames and window utilities =================================================
 
 (use-package transpose-frame)
 
-(use-package ace-window
-  :bind ("M-o" . ace-window))
+(use-package windmove
+  :ensure nil
+  :hook (after-init . windmove-mode)
+  :bind (("M-<up>" . windmove-up)
+         ("M-<right>" . windmove-right)
+         ("M-<down>" . windmove-down)
+         ("M-<left>" . windmove-left)
+         ("S-M-<up>" . windmove-swap-states-up)
+         ("S-M-<right>" . windmove-swap-states-right) ; conflicts with `org-increase-number-at-point'
+         ("S-M-<down>" . windmove-swap-states-down)
+         ("S-M-<left>" . windmove-swap-states-left))
+  )
 
 (use-package auto-dark
   :hook ((auto-dark-dark-mode . chbm-set-fonts)
@@ -239,6 +477,17 @@ function.  Then you can control the buffer's specifics via
 (use-package which-key
   :ensure nil
   :hook (after-init . which-key-mode))
+
+(use-package repeat
+  :ensure nil
+  :hook (after-init . repeat-mode)
+  :config
+  (setq repeat-on-final-keystroke t
+        repeat-exit-timeout 5
+        repeat-exit-key "<escape>"
+        repeat-keep-prefix nil
+        repeat-check-key t
+        set-mark-command-repeat-pop t))
 
 (use-package no-littering
   :demand t
