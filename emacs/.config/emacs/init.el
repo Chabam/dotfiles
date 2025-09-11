60933d141ed4442176c7f45e9bf4d005617bd1f5
Chabam <fchabot1337@gmail.com>
2025-09-11 15:00:49 -0400

    Baby's first elisp function 👶

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -25,6 +25,39 @@ function.  Then you can control the buffer's specifics via
   (pop-to-buffer buffer)
   (set-window-point (get-buffer-window buffer) (point-min)))
 
+(defun chbm-move-window-in-direction (dir)
+  (condition-case error
+      (windmove-swap-states-in-direction dir)
+    (user-error
+     (let* ((sel-win (selected-window))
+            (root-win (frame-root-window))
+            (sel-win-par (window-parent sel-win)))
+       (if (eq root-win sel-win-par)
+           (user-error (concat "Cannot move window "
+                               (symbol-name dir)
+                               ", window is alread at the edge"))
+         (let* ((width (window-total-width sel-win))
+                (height (window-total-height sel-win))
+                (new-win (split-window (window-parent sel-win) nil dir)))
+           (delete-window sel-win)
+           (select-window new-win)))))))
+
+(defun chbm-move-window-right ()
+  (interactive)
+  (chbm-move-window-in-direction 'right))
+
+(defun chbm-move-window-left ()
+  (interactive)
+  (chbm-move-window-in-direction 'left))
+
+(defun chbm-move-window-above ()
+  (interactive)
+  (chbm-move-window-in-direction 'above))
+
+(defun chbm-move-window-below ()
+  (interactive)
+  (chbm-move-window-in-direction 'below))
+
 
 ;; Main emacs config  ==========================================================
 
@@ -458,11 +491,15 @@ than `split-width-threshold'."
 (use-package windmove
   :ensure nil
   :hook (after-init . windmove-mode)
+  :bind (("C-S-<right>" . chbm-move-window-right)
+         ("C-S-<left>" . chbm-move-window-left)
+         ("C-S-<up>" . chbm-move-window-above)
+         ("C-S-<>" . chbm-move-window-below))
   :config
   (windmove-default-keybindings 'ctrl)
-  (windmove-swap-states-default-keybindings '(ctrl shift))
+  ;; (windmove-swap-states-default-keybindings '(ctrl shift))
   (windmove-delete-default-keybindings)
-  (setq windmove-wrap-around t))
+  )
 
 (use-package auto-dark
   :hook ((auto-dark-dark-mode . chbm-set-fonts)
