764e9c40090de8e9671992cdad405ed23574816b
Chabam <fchabot1337@gmail.com>
2025-09-15 21:33:12 -0400

    Trying out tramp defaults

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -745,21 +745,9 @@ than `split-width-threshold'."
   (setq remote-file-name-inhibit-locks t
         tramp-use-scp-direct-remote-copying t
         remote-file-name-inhibit-auto-save-visited t
-        tramp-copy-size-limit (* 1024 1024) ;; 1MB
-        tramp-verbose 2)
-
-  (connection-local-set-profile-variables
-   'remote-direct-async-process
-   '((tramp-direct-async-process . t)))
-
-  (connection-local-set-profiles
-   '(:application tramp :protocol "scp")
-   'remote-direct-async-process)
-
-  (setq magit-tramp-pipe-stty-settings 'pty)
-  (with-eval-after-load 'tramp
-    (with-eval-after-load 'compile
-      (remove-hook 'compilation-mode-hook #'tramp-compile-disable-ssh-controlmaster-options))))
+        tramp-copy-size-limit (* 1024 1024) ;; 1mb
+        tramp-verbose 2
+        enable-remote-dir-locals t))
 
 ;; Editor enhancements =========================================================
 
