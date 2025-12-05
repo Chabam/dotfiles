eefe8b41d2fc92a39b35f7ed558e1ccab5028c96
Chabam <fchabot1337@gmail.com>
2025-12-05 17:08:32 -0500

    Increased emacs' gc limit

---
+++ emacs/.config/emacs/early-init.el
@@ -0,0 +1,9 @@
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.5)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 100 100 8)
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist prot-emacs--file-name-handler-alist
+                  vc-handled-backends prot-emacs--vc-handled-backends)))
