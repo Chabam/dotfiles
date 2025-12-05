bb87a7e5ea697e087cc6f17cbe76135d0a4fa524
Chabam <fchabot1337@gmail.com>
2025-12-05 17:17:29 -0500

    MOARRRR

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -3,7 +3,7 @@
 
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold (* 100 100 8)
+            (setq gc-cons-threshold (* 50 1024 1024)
                   gc-cons-percentage 0.1
                   file-name-handler-alist prot-emacs--file-name-handler-alist
                   vc-handled-backends prot-emacs--vc-handled-backends)))
