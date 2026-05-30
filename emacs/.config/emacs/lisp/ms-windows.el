4be882eafb728ee86bc2d1e87a680234d35172bf
chabam <fchabot1337@gmail.com>
2026-05-29 20:33:04 -0400

    Fixed Windows emacsclient

--- emacs/.config/emacs/lisp/ms-windows.el
+++ emacs/.config/emacs/lisp/ms-windows.el
@@ -1,3 +1,8 @@
+;; Needed for some reason
+(setq server-auth-dir (file-name-concat user-emacs-directory "server"))
+(unless (file-exists-p server-auth-dir)
+  (make-directory server-auth-dir t))
+
 (when (member "Segoe UI Emoji" (font-family-list))
   (set-fontset-font t 'emoji (font-spec :family "Segoe UI Emoji") nil 'prepend))
 
