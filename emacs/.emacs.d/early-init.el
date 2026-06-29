d7ef10a461b57c4e2fdf15400c9bca74cc8d3d9c
Chabam <fchabot1337@gmail.com>
2026-06-29 12:24:31 -0400

    Making use of the default gc thresholds

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,16 +1,18 @@
-(setq gc-cons-threshold most-positive-fixnum)
-(setq gc-cons-percentage 1.0)
+(let ((default-gc-thresh gc-cons-threshold)
+      (default-gc-percent gc-cons-percentage))
+  (setq gc-cons-threshold most-positive-fixnum)
+  (setq gc-cons-percentage 1.0)
 
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "Emacs ready in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
+  (add-hook 'emacs-startup-hook
+            (lambda ()
+              (message "Emacs ready in %s with %d garbage collections."
+                       (format "%.2f seconds"
+                               (float-time
+                                (time-subtract after-init-time before-init-time)))
+                       gcs-done)))
 
-(run-with-idle-timer
- 5 nil
- (lambda ()
-   (setq gc-cons-threshold (* 32 1024 1024))
-   (setq gc-cons-percentage 0.1)))
+  (run-with-idle-timer
+   5 nil
+   (lambda ()
+     (setq gc-cons-threshold default-gc-thresh)
+     (setq gc-cons-percentage default-gc-percent))))
