76e167e2640df5f62111d11d6dc58066b6a37123
Chabam <fchabot1337@gmail.com>
2026-07-08 15:57:55 -0400

    Nah I don't need ghostel, shell is fine

--- emacs/.emacs.d/configs/appearance.el
+++ emacs/.emacs.d/configs/appearance.el
@@ -29,11 +29,85 @@
 (setq tab-bar-new-button-show nil)
 (setq tab-bar-show 1)
 
+(use-package rainbow-mode
+  :ensure nil
+  :config
+  (setq rainbow-x-colors nil))
+
 (use-package modus-themes
   :ensure t
   :init
   (custom-set-faces
    '(variable-pitch ((t (:family "Iosevka")))))
+
+  ;; From https://gitlab.gnome.org/chergert/ptyxis/-/blob/cbdee77429b8a4b70f1bea95af559eeddbb4ee17/src/palettes/gnome.palette
+  (setq modus-vivendi-palette-overrides
+        '((fg-term-black          "#241F31")
+          (bg-term-black          fg-term-black)
+          (fg-term-red            "#C01C28")
+          (bg-term-red            fg-term-red)
+          (fg-term-green          "#2EC27E")
+          (bg-term-green          fg-term-green)
+          (fg-term-yellow         "#F5C211")
+          (bg-term-yellow         fg-term-yellow)
+          (fg-term-blue           "#1E78E4")
+          (bg-term-blue           fg-term-blue)
+          (fg-term-magenta        "#9841BB")
+          (bg-term-magenta        fg-term-magenta)
+          (fg-term-cyan           "#0AB9DC")
+          (bg-term-cyan           fg-term-cyan)
+          (fg-term-white          "#C0BFBC")
+          (bg-term-white          fg-term-white)
+          (fg-term-black-bright   "#5E5C64")
+          (bg-term-black-bright   fg-term-black-bright)
+          (fg-term-red-bright     "#ED333B")
+          (bg-term-red-bright     fg-term-red-bright)
+          (fg-term-green-bright   "#57E389")
+          (bg-term-green-bright   fg-term-green-bright)
+          (fg-term-yellow-bright  "#F8E45C")
+          (bg-term-yellow-bright  fg-term-yellow-bright)
+          (fg-term-blue-bright    "#51A1FF")
+          (bg-term-blue-bright    fg-term-blue-bright)
+          (fg-term-magenta-bright "#C061CB")
+          (bg-term-magenta-bright fg-term-magenta-bright)
+          (fg-term-cyan-bright    "#4FD2FD")
+          (bg-term-cyan-bright    fg-term-cyan-bright)
+          (fg-term-white-bright   "#F6F5F4")
+          (bg-term-white-bright   fg-term-white-bright)))
+
+  (setq modus-operandi-palette-overrides
+        '((fg-term-black          "#1d1d20")
+          (bg-term-black          fg-term-black)
+          (fg-term-red            "#c01c28")
+          (bg-term-red            fg-term-red)
+          (fg-term-green          "#26a269")
+          (bg-term-green          fg-term-green)
+          (fg-term-yellow         "#a2734c")
+          (bg-term-yellow         fg-term-yellow)
+          (fg-term-blue           "#12488b")
+          (bg-term-blue           fg-term-blue)
+          (fg-term-magenta        "#a347ba")
+          (bg-term-magenta        fg-term-magenta)
+          (fg-term-cyan           "#2aa1b3")
+          (bg-term-cyan           fg-term-cyan)
+          (fg-term-white          "#cfcfcf")
+          (bg-term-white          fg-term-white)
+          (fg-term-black-bright   "#5d5d5d")
+          (bg-term-black-bright   fg-term-black-bright)
+          (fg-term-red-bright     "#f66151")
+          (bg-term-red-bright     fg-term-red-bright)
+          (fg-term-green-bright   "#33d17a")
+          (bg-term-green-bright   fg-term-green-bright)
+          (fg-term-yellow-bright  "#e9ad0c")
+          (bg-term-yellow-bright  fg-term-yellow-bright)
+          (fg-term-blue-bright    "#2a7bde")
+          (bg-term-blue-bright    fg-term-blue-bright)
+          (fg-term-magenta-bright "#c061cb")
+          (bg-term-magenta-bright fg-term-magenta-bright)
+          (fg-term-cyan-bright    "#33c7de")
+          (bg-term-cyan-bright    fg-term-cyan-bright)
+          (fg-term-white-bright   "#ffffff")
+          (bg-term-white-bright   fg-term-white-bright)))
 
   (setq modus-themes-common-palette-overrides
       `(
@@ -76,7 +150,7 @@
         (date-weekday cyan-cooler)
         (date-weekend blue-faint)))
 
-(setq modus-themes-variable-pitch-ui nil
+  (setq modus-themes-variable-pitch-ui nil
       modus-themes-italic-constructs nil
       modus-themes-bold-constructs t
       modus-themes-mixed-fonts nil))
@@ -84,7 +158,7 @@
 (use-package auto-dark
   :ensure t
   :defer t
-  :hook (after-init . auto-dark-mode)
+  :hook ((after-init . auto-dark-mode))
   :custom
   (auto-dark-themes '((modus-vivendi) (modus-operandi))))
 
@@ -131,6 +205,5 @@ function.  Then you can control the buffer's specifics via
 ;; Whitespace
 (setq whitespace-style '(face indentation tabs tab-mark spaces space-mark
                               newline newline-mark trailing))
-
 
 (provide 'appearance)
