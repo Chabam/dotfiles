5af4bef1f3c6edf58a4c69de10b0e9e1b8a56994
Chabam <fchabot1337@gmail.com>
2026-07-10 11:12:14 -0400

    Reorg and cleaning up

--- flatpak/flatpak-list.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-flatpak list --app --columns=ref,installation | tail -n +1 > flatpaks.txt
--- flatpak/install-flatpaks.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-awk '{ system("flatpak --$2 install "$1) }' flatpaks.txt
