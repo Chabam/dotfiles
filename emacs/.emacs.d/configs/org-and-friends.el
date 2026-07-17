87c7c84d6aacfb7b9174ece2fa35e836c3741808
Chabam <fchabot1337@gmail.com>
2026-07-17 11:30:00 -0400

    Reorganizing org config

--- emacs/.emacs.d/configs/org-and-friends.el
+++ emacs/.emacs.d/configs/org-and-friends.el
@@ -37,87 +37,48 @@
     (org-agenda nil "d")
     (delete-other-windows)))
 
-(use-package org
+(use-package ox
   :ensure nil
-  :bind (("C-c l" . org-store-link)
-         ("C-c a" . org-agenda)
-         ("C-c c" . org-capture))
-  :hook ((org-mode . (lambda ()
-                       (add-hook 'completion-at-point-functions #'cape-file nil t)))
-         (org-mode . chbm/set-website-config))
+  :commands (org-export-dispatch org-export-to-file)
   :config
-  (require 'ox-publish)
 
-  (setq org-directory "~/Documents/Org"
-        org-icalendar-exclude-tags '("noexport")
-        org-icalendar-combined-agenda-file (file-name-concat org-directory "org.ics")
-        org-icalendar-use-scheduled '(event-if-not-todo event-if-todo event-if-todo-not-done todo-start)
-        org-icalendar-use-deadline '(event-if-not-todo event-if-todo event-if-todo-not-done todo-due)
-        org-icalendar-timezone "America/Toronto"
-        org-icalendar-date-time-format ";TZID=%Z:%Y%m%dT%H%M%S"
+  (setq org-export-with-date nil)
+  (setq org-export-with-author nil)
+  (setq org-export-time-stamp-file nil)
+  (setq org-export-allow-bind-keywords t)
+  (setq org-export-with-broken-links t))
 
-        icalendar-export-sexp-enumerate-all t
-        icalendar-export-sexp-enumeration-days 365
+(use-package ox-icalendar
+  :ensure nil
+  :after ox
+  :commands (org-icalendar-export-to-ics)
+  :config
+  (setq org-icalendar-exclude-tags '("noexport"))
+  (setq org-icalendar-combined-agenda-file "org.ics")
+  (setq org-icalendar-use-scheduled '(event-if-not-todo event-if-todo event-if-todo-not-done todo-start))
+  (setq org-icalendar-use-deadline '(event-if-not-todo event-if-todo event-if-todo-not-done todo-due))
+  (setq org-icalendar-timezone "America/Toronto")
+  (setq org-icalendar-date-time-format ";TZID=%Z:%Y%m%dT%H%M%S")
 
-        org-src-lang-modes `(("C" . c-ts)
-                             ("C++" . c++-ts)
-                             ("asymptote" . asy)
-                             ("beamer" . latex)
-                             ("calc" . fundamental)
-                             ("cpp" . c++-ts)
-                             ("ditaa" . artist)
-                             ("desktop" . conf-desktop)
-                             ("dot" . fundamental)
-                             ("elisp" . emacs-lisp)
-                             ("ocaml" . tuareg)
-                             ("screen" . shell-script)
-                             ("sqlite" . sql)
-                             ("toml" . toml-ts)
-                             ("shell" . sh)
-                             ,@(org-src--get-known-shells))
+  (setq icalendar-export-sexp-enumerate-all t)
+  (setq icalendar-export-sexp-enumeration-days 365))
 
-        org-babel-results-keyword "results"
+(use-package ox-html
+  :ensure nil
+  :commands (org-html-export-to-html)
+  :after ox
+  :config
 
-        org-export-with-date nil
-        org-export-with-author nil
-        org-export-time-stamp-file nil
-        org-export-allow-bind-keywords t
-        org-export-with-broken-links t
+  (setq org-html-head chabam-ca-headers)
+  (setq org-html-style-default nil)
+  (setq org-html-htmlize-output-type 'css)
+  (setq org-html-validation-link nil))
 
-        org-html-head chabam-ca-headers
-        org-html-style-default nil
-        org-html-htmlize-output-type 'css
-        org-html-validation-link nil
-
-        org-attach-use-inheritance t
-        org-attach-auto-tag nil
-
-        org-default-notes-file (file-name-concat org-directory "inbox.org")
-        org-capture-templates '(("t" "Tâche" entry (file "")
-		                         "* FAIRE %?\n  %u\n  %a"))
-        org-agenda-files (list (file-name-concat org-directory "agenda.org")
-                               (file-name-concat org-directory "inbox.org")
-                               (file-name-concat org-directory "notes.org"))
-        org-archive-location (concat org-directory "/archive.org::datetree/")
-        org-attach-id-dir (file-name-concat org-directory "data")
-        org-todo-keywords '((sequence "FAIRE(f)" "COURS(c)" "ATTENTE(a)" "FAIT(F)")))
-
-  (when chbm/emacs-containerized
-    (setq org-file-apps '((auto-mode . emacs)
-                          (directory . emacs)
-                          ("\\.mm\\'" . chbm/xdg-open-host)
-                          ("\\.x?html?\\'" . chbm/xdg-open-host)
-                          ("\\.pdf\\'" . chbm/xdg-open-host))))
-  ;; babel
-  (org-babel-do-load-languages
-   'org-babel-load-languages '((C . t)
-                               (emacs-lisp . t)
-                               (haskell . t)
-                               (js . t)
-                               (python . t)
-                               (R . t)
-                               (shell . t)))
-  ;; calendar
+(use-package org-agenda
+  :ensure nil
+  :bind (("C-c a" . org-agenda))
+  :config
+  (setq org-agenda-files '("agenda.org" "inbox.org" "notes.org"))
   (add-to-list 'org-agenda-custom-commands
                '("W" "Weekend"
                  ((agenda ""))
@@ -134,6 +95,65 @@
                   (alltodo "")))
                t))
 
+(use-package org
+  :ensure nil
+  :bind (("C-c l" . org-store-link)
+         ("C-c c" . org-capture))
+  :hook ((org-mode . (lambda ()
+                       (add-hook 'completion-at-point-functions #'cape-file nil t)))
+         (org-mode . chbm/set-website-config))
+  :config
+
+  (setq org-directory "~/Documents/Org")
+  (setq org-src-lang-modes `(("C" . c-ts)
+                             ("C++" . c++-ts)
+                             ("asymptote" . asy)
+                             ("beamer" . latex)
+                             ("calc" . fundamental)
+                             ("cpp" . c++-ts)
+                             ("ditaa" . artist)
+                             ("desktop" . conf-desktop)
+                             ("dot" . fundamental)
+                             ("elisp" . emacs-lisp)
+                             ("ocaml" . tuareg)
+                             ("screen" . shell-script)
+                             ("sqlite" . sql)
+                             ("toml" . toml-ts)
+                             ("shell" . sh)
+                             ,@(org-src--get-known-shells)))
+
+  (setq org-babel-results-keyword "results")
+
+  (setq org-attach-use-inheritance t)
+
+  (setq org-default-notes-file "inbox.org")
+  (setq org-capture-templates '(("t" "Tâche" entry (file ""))
+		                         "* FAIRE %?\n%u\n%a"))
+
+  (setq org-archive-location "archive.org::datetree/")
+  (setq org-attach-id-dir "data")
+  (setq org-todo-keywords '((sequence "FAIRE(f)" "COURS(c)" "ATTENTE(a)" "FAIT(F)")))
+
+  (setq org-refile-use-outline-path 'file)
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
+
+  (when chbm/emacs-containerized
+    (setq org-file-apps '((auto-mode . emacs)
+                          (directory . emacs)
+                          ("\\.mm\\'" . chbm/xdg-open-host)
+                          ("\\.x?html?\\'" . chbm/xdg-open-host)
+                          ("\\.pdf\\'" . chbm/xdg-open-host))))
+  ;; babel
+  (org-babel-do-load-languages
+   'org-babel-load-languages '((C . t)
+                               (emacs-lisp . t)
+                               (haskell . t)
+                               (js . t)
+                               (python . t)
+                               (R . t)
+                               (shell . t))))
+
 ;; (use-package citar
 ;;   :ensure t
 ;;   :after org
@@ -142,9 +162,9 @@
 ;;                             org-cite-follow-processor 'citar
 ;;                             org-cite-activate-processor 'citar))))
 
-(use-package htmlize
-  :ensure t
-  :commands (org-export-dispatch))
+;; (use-package htmlize
+;;   :ensure t
+;;   :commands (org-export-dispatch))
 
 (use-package org-download
   :ensure t
@@ -166,10 +186,10 @@
   (setq org-caldav-calendar-id "chabam/main")
   (setq org-caldav-sync-todo t)
   (setq org-icalendar-include-todo 'all)
-  (setq org-caldav-inbox (file-name-concat org-directory "inbox.org"))
+  (setq org-caldav-inbox "inbox.org")
   (setq org-caldav-files (list org-caldav-inbox
-                               (file-name-concat org-directory "agenda.org")
-                               (file-name-concat org-directory "archive.org")))
+                               "agenda.org"
+                               "archive.org"))
   (setq org-caldav-save-directory (file-name-concat org-directory ".org-caldav"))
   (setq org-caldav-todo-percent-states
         '((0 "FAIRE") (25 "COURS") (50 "ATTENTE") (100 "FAIT"))))
